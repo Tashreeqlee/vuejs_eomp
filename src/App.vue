d19680b6c5f2ed3adf91756c7b5ae8b17a44646d
Tashreeqlee <tashyct14@gmail.com>
2024-01-08 11:25:32 +0200

    added navbar and footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <Navbar/>
+  <main class="container-fluid">
   <router-view/>
+  </main>
+  <Footer/>
 </template>
+<script>
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
+
+
+
+export default {
+  components:{
+    Navbar,
+    Footer
+  }
+
+}
+</script>
 
 <style>
 #app {
